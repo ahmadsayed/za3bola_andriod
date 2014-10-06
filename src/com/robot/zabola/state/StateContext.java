e0746cd989af45bdd39b11fb32303f13258d5959
ahmed <ahmad.ahmadsayed@gmail.com>
2014-10-06 04:23:53 +0200

    Test commit from eclipse

--- src/com/robot/zabola/state/StateContext.java
+++ src/com/robot/zabola/state/StateContext.java
@@ -8,7 +8,7 @@ public class StateContext {
 	
 	private Statelike myState;
     
-    public StateContext () {    	
+    public StateContext () {
     }
     
     public StateContext(Statelike state) {    	
